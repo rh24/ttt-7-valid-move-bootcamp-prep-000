50439584a76831081b3b6387f321348b8ab77df7
Rebecca Hong <ms.rebeccahong@gmail.com>
2017-10-04 12:06:21 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,6 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
-  position_taken?(board, index) == false && index.between?(0, 8) == true ? true : false
+  !position_taken(board, index) && index.between?(0, 8) == true ? true : false
   position_taken?(board, index) == true  || index.between?(0, 8) == false ? false : true
 end
 
